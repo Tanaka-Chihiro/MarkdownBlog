3185f2daddb32d895be197941ea7020d753e06b5
Tanaka Chihiro <tokyo.9st@gmail.com>
2024-11-26 20:44:10 +0900

    commit

--- src/app/components/Pagination.tsx
+++ src/app/components/Pagination.tsx
@@ -10,11 +10,9 @@ const Pagination = ({ type, pages, currentPage = 1 }: PageProps) => {
   const totalPages = pages.length;
   const pageLimit = 5;
 
-  // 計算した開始ページと終了ページを決定
   let startPage = Math.max(currentPage - Math.floor(pageLimit / 2), 1);
-  let endPage = Math.min(startPage + pageLimit - 1, totalPages);
+  const endPage = Math.min(startPage + pageLimit - 1, totalPages);
 
-  // ページ数が足りない場合は調整
   if (endPage - startPage + 1 < pageLimit) {
     startPage = Math.max(endPage - pageLimit + 1, 1);
   }
